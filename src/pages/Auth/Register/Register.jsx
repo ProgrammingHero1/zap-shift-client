7689290df51f3814bdec7aa9f98610b283bbeb1e
Programming Hero <programming.hero1@gmail.com>
2025-11-15 07:23:01 -0600

    created login page and custom auth hook

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import useAuth from '../../../hooks/useAuth';
 
 const Register = () => {
-
     const { register, handleSubmit, formState: { errors } } = useForm();
+    const { registerUser } = useAuth();
 
     const handleRegistration = (data) => {
         console.log('after register', data);
+        registerUser(data.email, data.password)
+            .then(result => {
+                console.log(result.user);
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
 
     return (
@@ -25,7 +33,7 @@ const Register = () => {
                         pattern: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).+$/
                     })} className="input" placeholder="Password" />
                     {
-                        errors.password?.type==='required' && <p className='text-red-500'>Password is required.</p>
+                        errors.password?.type === 'required' && <p className='text-red-500'>Password is required.</p>
                     }
                     {
                         errors.password?.type === 'minLength' && <p className='text-red-500'>
@@ -33,11 +41,11 @@ const Register = () => {
                         </p>
                     }
                     {
-                        errors.password?.type==='pattern' && <p className='text-red-500'>Password must have at least one uppercase, at least one lowercase, at least one number, and at least one special characters</p>
+                        errors.password?.type === 'pattern' && <p className='text-red-500'>Password must have at least one uppercase, at least one lowercase, at least one number, and at least one special characters</p>
                     }
 
                     <div><a className="link link-hover">Forgot password?</a></div>
-                    <button className="btn btn-neutral mt-4">Login</button>
+                    <button className="btn btn-neutral mt-4">Register</button>
                 </fieldset>
             </form>
         </div>
