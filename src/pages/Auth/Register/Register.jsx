05802d054c07648774fa609fe927fe1332eeab9b
Programming Hero <programming.hero1@gmail.com>
2025-11-15 12:40:13 -0600

    get image for registration

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -9,10 +9,16 @@ const Register = () => {
     const { registerUser } = useAuth();
 
     const handleRegistration = (data) => {
-        console.log('after register', data);
+        
+        console.log('after register', data.photo[0]);
+
         registerUser(data.email, data.password)
             .then(result => {
                 console.log(result.user);
+                // store the image and get the photo url
+                
+                // update user profile 
+
             })
             .catch(error => {
                 console.log(error)
@@ -25,6 +31,19 @@ const Register = () => {
             <p className='text-center'>Please Register</p>
             <form className="card-body" onSubmit={handleSubmit(handleRegistration)}>
                 <fieldset className="fieldset">
+                    {/* name field */}
+                    <label className="label">Name</label>
+                    <input type="text" {...register('name', { required: true })} className="input" placeholder="Your Name" />
+                    {errors.name?.type === 'required' && <p className='text-red-500'>Name is required.</p>}
+                    
+                    {/* photo image field */}
+                    <label className="label">Photo</label>
+                    
+                    <input type="file" {...register('photo', { required: true })} className="file-input" placeholder="Your Photo" />
+
+                    {errors.name?.type === 'required' && <p className='text-red-500'>Photo is required.</p>}
+
+                    {/* email field */}
                     <label className="label">Email</label>
                     <input type="email" {...register('email', { required: true })} className="input" placeholder="Email" />
                     {errors.email?.type === 'required' && <p className='text-red-500'>Email is required.</p>}
